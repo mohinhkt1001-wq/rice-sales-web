8c644f8b5b80c28ef39622953cb356f3ea8b5cd2
mohinhkt1001-wq <mohinhkt1001@gmail.com>
2026-01-15 22:44:29 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzk63ZP43XRP3Ej_1iR6ywLzk6AsTjZNawDH7ga1urlIhykN-2mQr7OVqqZtN1jHHzn/exec";
 
+window.onload = function() {
+  loadRiceList();
+}
+function loadRiceList() {
+  fetch(WEB_APP_URL)
+    .then(res => res.json())
+    .then(list => {
+      const select = document.getElementById("rice");
+      select.innerHTML = '<option value="">-- Chọn gạo --</option>';
+
+      list.forEach(item => {
+        const opt = document.createElement("option");
+        opt.value = item.ma;
+        opt.text = item.ten + " (" + item.gia + " đ/kg)";
+        opt.dataset.price = item.gia;
+        select.appendChild(opt);
+      });
+    })
+    .catch(err => {
+      console.error("Không load được danh sách gạo", err);
+    });
+}
+
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzk63ZP43XRP3Ej_1iR6ywLzk6AsTjZNawDH7ga1urlIhykN-2mQr7OVqqZtN1jHHzn/exec";
+
 function addSale() {
   const maGao = document.getElementById("rice").value;
   const khoiLuong = document.getElementById("qty").value;

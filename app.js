4bf34339841ee12cdf526c74c62d47d08c4c0729
mohinhkt1001-wq <mohinhkt1001@gmail.com>
2026-01-15 23:34:37 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-// ===== LINK WEB APP GOOGLE SCRIPT =====
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbzk63ZP43XRP3Ej_1iR6ywLzk6AsTjZNawDH7ga1urlIhykN-2mQr7OVqqZtN1jHHzn/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbz63ZP43XRP3Ej_iR6ywLzK6AsTjZNawDH7galurIhykN-2mQr70VQqzTN1jHhnZn/exec";
 
-// ===== LOAD DANH SÁCH GẠO =====
 function loadRiceList() {
   fetch(WEB_APP_URL)
     .then(res => res.json())
     .then(data => {
       const select = document.getElementById("rice");
-      select.innerHTML = '<option value="">-- Chọn loại gạo --</option>';
+      select.innerHTML = '<option value="">-- Chọn gạo --</option>';
 
       data.forEach(item => {
         const opt = document.createElement("option");
@@ -17,8 +15,7 @@ function loadRiceList() {
         select.appendChild(opt);
       });
     })
-    .catch(err => console.error("Lỗi load danh sách gạo:", err));
+    .catch(err => console.error("Lỗi load gạo:", err));
 }
 
-// ===== AUTO LOAD KHI MỞ TRANG =====
 window.onload = loadRiceList;
